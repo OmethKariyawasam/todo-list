a65042f2d19704ee79f03d872440f3b0b4fb2ea0
OmethKariyawasam <159032631+OmethKariyawasam@users.noreply.github.com>
2024-08-11 20:08:01 +0530

    Add get request and css styles

--- server/index.js
+++ server/index.js
@@ -11,6 +11,12 @@ app.use(express.json())
 
 mongoose.connect(uri)
 
+app.get('/get', (req, res) => {
+    TodoModel.find()
+    .then(result => res.json(result))
+    .catch(err => res.json(err))
+})
+
 app.post('/add', (req, res) =>{
     const task =req.body.task;
     TodoModel.create({
--- todolist/src/Home.jsx
+++ todolist/src/Home.jsx
@@ -1,8 +1,14 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Create from './Create'
+import axios from 'axios'
 
 function Home() {
     const [todos, setTodos] = useState([])
+    useEffect(() => {
+        axios.get('http://localhost:3001/get')
+        .then(result => setTodos(result.data))
+        .catch(err => console.log(err))
+    })
   return (
     <div className='home'>
         <h2>Todo List</h2>
@@ -12,8 +18,8 @@ function Home() {
             <div><h2>No Record</h2></div>
             :
             todos.map(todo => (
-                <div>
-                    {todo}
+                <div className='task'>
+                    <p>{todo.task}</p>
                 </div>
             ))
         }
